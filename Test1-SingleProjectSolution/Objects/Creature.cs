add2cac3e2c20d4b76ff62892d53b43c2790e8ab
james-eai <james@eaiti.com>
2019-05-10 16:54:29 -0400

    added constructor

--- Test1-SingleProjectSolution/Objects/Creature.cs
+++ Test1-SingleProjectSolution/Objects/Creature.cs
@@ -12,6 +12,10 @@ namespace Test1_SingleProjectSolution.Objects
 		public int NumberOfEars { get; set; }
 		public int NumberOfTeeth { get; set; }
 		public string Designation { get; set; }
+		public bool HasBrain { get; set; }
+		public bool IsAlive { get; set; }
 		public bool IsHuman { get; set; }
+		public Creature(int NumberOfArms = 0, int NumberOfEars = 0, int NumberOfEyes = 0, int NumberOfLegs = 0, int NumberOfTeeth = 0, bool IsHuman = false, bool HasBrain = false, bool IsAlive = false, string Designation = "Your creature has no designation.")
+		{ }
 	}
 }
