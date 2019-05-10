280218063d0f87602a37aff76a109e4f0d21ecda
james-eai <james@eaiti.com>
2019-05-10 16:50:22 -0400

    Initial creature commit.

---
+++ Test1-SingleProjectSolution/Objects/Creature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test1_SingleProjectSolution.Objects
+{
+	class Creature
+	{
+		public int NumberOfLegs { get; set; }
+		public int NumberOfEyes { get; set; }
+		public int NumberOfArms { get; set; }
+		public int NumberOfEars { get; set; }
+		public int NumberOfTeeth { get; set; }
+		public string Designation { get; set; }
+		public bool IsHuman { get; set; }
+	}
+}
