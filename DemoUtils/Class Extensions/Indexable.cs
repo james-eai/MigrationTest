1158b98df11d4931abc98e3740e81a014c918918
james <james@eaiti.com>
2019-05-10 09:05:10 -0400

    Added Utils Project

---
+++ DemoUtils/Class Extensions/Indexable.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace DemoUtils
+{
+	public abstract class Indexable
+	{
+		public int KeyID { get; set; }
+		public object this[int index]
+		{
+			get => GetType().GetProperties()[index].GetValue(this);
+			set => GetType().GetProperties()[index].SetValue(this, value);
+		}
+	}
+}
